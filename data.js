a8813974ab17b05c1fe4cb4a995b99a8cbc64210
Raihan-Zidan <105942900+Raihan-Zidan@users.noreply.github.com>
2025-02-26 19:44:00 +0700

    Update data.js

--- data.js
+++ data.js
@@ -305,7 +305,8 @@ function getCorsHeaders() {
   return {
     "Content-Type": "application/json",
     "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET, OPTIONS",
-    "Access-Control-Allow-Headers": "Content-Type",
+    "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type, Authorization",
   };
 }
+
